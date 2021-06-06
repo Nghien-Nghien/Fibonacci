811d73255f1851dd4eb5bf960e4be7866051f306
Nghien-Nghien <nguyenhoangsang28091995@gmail.com>
2021-06-06 22:24:59 +0700

    last commit

--- src/Fibonaccy.java
+++ src/Fibonaccy.java
@@ -1,13 +1,12 @@
 public class Fibonaccy {
-        public static void main(String[] args) {
+        static void Testing() {
             System.out.println("Day so Fibonacci:");
             int n1 = 0;
             int n2 = 1;
             int n3;
-            int count;
+            int count= 26;
 
             String series="";
-            count=26;
             for (int i = 2; i < count; ++i) { //loop starts from 2 because 0 and 1 are already printed//2,3,4,5,6
                 n3 = n1 + n2;//n3=0+1=1;1+1=2;1+2=3;2+3=5;3+5=8;
                 series += n3 + " ";//s=1 ;=1 2 ;=1 2 3 ;
@@ -15,5 +14,8 @@ public class Fibonaccy {
                 n2 = n3;//n2=1;2;3;5;8;
             }
             System.out.print(0 + " " + 1 + " " + series);//s=1 ;=1 2 ;=1 2 3 ;
+        }
+        public static void main (String[] args) {
+            Testing();
         }
 }
